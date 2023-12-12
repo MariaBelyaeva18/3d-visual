6e8ce38747f1834cc1d7278feaa9152e4cea2956
MariaBelyaeva18 <142314456+MariaBelyaeva18@users.noreply.github.com>
2023-12-12 23:37:55 +0300

    start

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+
+
+<template>
+  <Header>
+    <HelloWorld/>
+  </Header>
+
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Header from "./layout/Header.vue";
+</script>
---
+++ frontend/src/layout/Header.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+    <header>
+      Natalia Belyaeva
+          <a href="#">Обо мне</a>
+          <a href="#">Портфолио</a>
+          <a href="#">Контакты</a>
+    </header>
+
+    <slot />
+  </section>
+
+</template>
+
+<style>
+
+
+</style>
